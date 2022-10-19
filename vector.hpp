9324d2c0e06181949ba41f4ade90a2d35c680a13
Noe DORMOY <ndormoy@e1r3p3.clusters.42paris.fr>
2022-10-19 13:49:08 +0200

    todo list & stuff

--- vector.hpp
+++ vector.hpp
@@ -19,15 +19,15 @@ namespace	ft
 	class vector {
 		public:
 
-			// member types:
-			typedef T										value_type; //The type of the elements.
+			/*
+			member types:
+			*/
+			typedef T										value_type; // The type of the elements.
 			typedef Allocator								allocator_type;
-			typedef std::size_t								size_type;
-			typedef std::ptrdiff_t							difference_type;
-			typedef typename Allocator::reference			reference;
-			//ou typedef reference value_type&; // (sur cppreference)
+			typedef std::size_t								size_type; // Quantities of elements
+			typedef std::ptrdiff_t							difference_type; // Difference between two pointers
+			typedef typename Allocator::reference			reference;// Reference to element
 			typedef typename Allocator::const_reference		const_reference;
-			//ou typedef	typename const_reference value_type; // (sur cppreference) 
 			typedef typename Allocator::pointer				pointer;
 			typedef T										*iterator; // Pareil que d'heriter de std::random_access_iterator
 			typedef const T									*const_iterator; // Pareil que d'heriter de std::random_access_iterator
@@ -35,8 +35,15 @@ namespace	ft
 			typedef ft::reverse_iterator<iterator>			reverse_iterator;
 			typedef ft::reverse_iterator<const_iterator>	const_reverse_iterator;
 
-			// 23.2.4.1 construct/copy/destroy:
-			explicit vector(const Allocator& = Allocator()); //default constructor
+			/*
+			construct/copy/destroy:
+			*/
+
+			//default constructor
+			explicit vector(const Allocator& = Allocator()) :
+				_m_allocator(Allocator()), _m_size(0), _m_capacity(0), _m_begin(NULL), _m_end(NULL) {}
+			{
+			}
 			explicit vector(size_type n, const T& value = T(), const Allocator& = Allocator()); //fill constructor
 			template <class InputIterator>
 			vector(InputIterator first, InputIterator last, const Allocator& = Allocator()); //range constructor
@@ -48,8 +55,15 @@ namespace	ft
 			void assign(size_type n, const T& u); //fill assign
 			allocator_type get_allocator() const; //get allocator
 
-			// iterators:
-			iterator begin(); //Returns an iterator pointing to the first element in the vector
+			/*
+			iterators:
+			*/
+
+			//Returns an iterator pointing to the first element in the vector
+			iterator begin()
+			{
+				return (_m_begin);
+			}
 			const_iterator begin() const; //Returns an const_iterator pointing to the first element in the vector
 			iterator end(); //Returns an iterator pointing to the theoretical element that follows the last element in the vector
 			const_iterator end() const; //Returns a const_iterator pointing to the theoretical element that follows the last element in the vector
@@ -58,16 +72,41 @@ namespace	ft
 			reverse_iterator rend(); //Returns a reverse iterator pointing to the theoretical element preceding the first element in the vector (considered as reverse end)
 			const_reverse_iterator rend() const; //Returns a const reverse iterator pointing to the theoretical element preceding the first element in the vector (considered as reverse end)
 
-			// 23.2.4.2 capacity:
-			size_type size() const; // Returns the number of elements in the vector.
-			size_type max_size() const; // Returns the maximum number of elements that the vector can hold.
+			/*
+			capacity:
+			*/
+
+			// Returns the number of elements in the vector.
+			size_type size() const
+			{
+				return	(_m_size);
+			}
+			// Returns the maximum number of elements that the vector can hold.
+			size_type max_size() const;
+			{
+				return (allocator_type::max_size());
+			}
 			// void resize(size_type sz, T c = T());
 			void	resize(size_type size, value_type val = value_type()); // Resizes the container so that it contains ‘size’ elements.
-			size_type capacity() const; // Returns the size of the storage space currently allocated to the vector expressed as number of elements.
-			bool empty() const; // Returns whether the container is empty.
+			// Returns the size of the storage space currently allocated to the vector expressed as number of elements.
+			size_type capacity() const
+			{
+				return (_m_capacity);
+			}
+			// Returns whether the container is empty.
+			bool empty() const
+			{
+				if (_m_size == 0)
+					return (true);
+				else
+					return (false);
+			}
 			void reserve(size_type n); // Requests that the vector capacity be at least enough to contain n elements.
 
-			// element access:
+			/*
+			element access:
+			*/
+
 			reference operator[](size_type n);
 			const_reference operator[](size_type n) const;
 			const_reference at(size_type n) const;
@@ -77,8 +116,18 @@ namespace	ft
 			reference back();
 			const_reference back() const;
 
-			// 23.2.4.3 modifiers:
-			void push_back(const T& x);
+			/*
+			modifiers:
+			*/
+
+			void push_back(const value_type& x)
+			{
+				if (_m_size < _m_capacity )
+				{
+					reserve( 2 * theCapacity + 1 );
+					_m_begin[ _m_size++ ] = x;
+				}
+			}
 			void pop_back();
 			iterator insert(iterator position, const T& x);
 			void insert(iterator position, size_type n, const T& x);
@@ -93,15 +142,18 @@ namespace	ft
 		private:
 
 			allocator_type	_m_allocator;//
-            size_type		_m_size; //size of the vector
-			size_type		_m_capacity; //a value at least as large as size
+            size_type		_m_size; // current number of stored elements
+			size_type		_m_capacity; //a value at least as large as size. Currently allocated storage space
 			pointer			_m_begin;//pointer to the first element of the vector
 			pointer 		_m_end;//pointer to the last element of the vector
 	
 	};
 
 	//ATTENTION : TO FILL
-	//overload operator of vector<T,Allocator>
+	/*
+	overload operator of vector<T,Allocator>
+	*/
+
 	template <class T, class Allocator>
 	bool operator==(const ft::vector<T,Allocator>& lhs, const ft::vector<T,Allocator>& rhs)
 	{	
