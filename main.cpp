5d4e9d330499d1f94df9372ec4b70aa8d72f6ca5
Noe DORMOY <ndormoy@e3r6p10.clusters.42paris.fr>
2022-10-26 17:24:48 +0200

    ALL test of Mazoise 42tester OK exept swap

--- main.cpp
+++ main.cpp
@@ -304,20 +304,20 @@ void	awesome_tests(void)
 
 int main()
 {
-	// push_pop_back_tests<int>();
-	// resize_tests<int>();
-	// insert_tests<int>();
-	// reserve_tests<int>();
-	// copy_swap_tests<int>();
-	// reverse_it_tests<int>();
-	// erase_clear_tests<int>();
-	// max_size_tests();
-	// awesome_tests();
-	// push_pop_back_tests<Awesome>();
-	// resize_tests<Awesome>();
-	// insert_tests<Awesome>();
-	// reserve_tests<Awesome>();
+	push_pop_back_tests<int>();
+	resize_tests<int>();
+	insert_tests<int>();
+	reserve_tests<int>();
+	copy_swap_tests<int>();
+	reverse_it_tests<int>();
+	erase_clear_tests<int>();
+	max_size_tests();
+	awesome_tests();
+	push_pop_back_tests<Awesome>();
+	resize_tests<Awesome>();
+	insert_tests<Awesome>();
+	reserve_tests<Awesome>();
 	copy_swap_tests<Awesome>();
-	// reverse_it_tests<Awesome>();
-	// erase_clear_tests<Awesome>();
+	reverse_it_tests<Awesome>();
+	erase_clear_tests<Awesome>();
 }
