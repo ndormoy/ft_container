292318ee44072b2f8be9074059a616e56a1b6181
Noe DORMOY <ndormoy@e3r6p10.clusters.42paris.fr>
2022-11-02 17:40:12 +0100

    Some clean

--- includes/red_black_tree.hpp
+++ includes/red_black_tree.hpp
@@ -278,66 +278,11 @@ namespace	ft
 					y->left->parent = y;
 					y->color = z->color;
 				}
-				// delete (z);
 				_allocator.destroy(z);
 				_allocator.deallocate(z, 1);
 				if (y_original_color == BLACK)
 					deleteFix(x);
 			}
-
-			// void deleteNodeHelper(NodePtr node, int key)
-			// {
-			// 	NodePtr z = TNULL;
-			// 	NodePtr x, y;
-			// 	while (node != TNULL) // find the node
-			// 	{
-			// 		if (node->data == key)
-			// 			z = node;
-			// 		if (node->data <= key)
-			// 			node = node->right;
-			// 		else
-			// 			node = node->left;
-			// 	}
-			// 	if (z == TNULL) // if node not found
-			// 	{
-			// 		std::cout << "Key not found in the tree" << std::endl;
-			// 		return ;
-			// 	}
-			// 	y = z;
-			// 	int	y_original_color = y->color;
-			// 	if (z->left == TNULL) // Swap the node with the right child because de the left child is NULL
-			// 	{
-			// 		x = z->right;
-			// 		rbTransplant(z, z->right);
-			// 	}
-			// 	else if (z->right == TNULL) // Swap the node with the left child because the right child is NULL
-			// 	{
-			// 		x = z->left;
-			// 		rbTransplant(z, z->left);
-			// 	}
-			// 	else
-			// 	{
-			// 		y = minimum(z->right);
-			// 		y_original_color = y->color;
-			// 		x = y->right;
-			// 		if (y->parent == z)
-			// 			x->parent = y;
-			// 		else
-			// 		{
-			// 			rbTransplant(y, y->right);
-			// 			y->right = z->right;
-			// 			y->right->parent = y;
-			// 		}
-
-			// 		rbTransplant(z, y);
-			// 		y->left = z->left;
-			// 		y->left->parent = y;
-			// 		y->color = z->color;
-			// 	}
-			// 	delete (z);
-			// 	if (y_original_color == BLACK)
-			// 		deleteFix(x);
-			// }
 
 			// https://algorithmtutor.com/Data-Structures/Tree/Red-Black-Trees/
 
@@ -586,48 +531,6 @@ namespace	ft
 	
 				insertFix(node);
 			}
-	
-			//// Inserting a node
-			// void insert(int key)
-			// {
-			// 	NodePtr node = new Node;
-			// 	node->parent = my_nullptr;
-			// 	node->data = key;
-			// 	node->left = TNULL;
-			// 	node->right = TNULL;
-			// 	node->color = RED;
-	
-			// 	NodePtr y = my_nullptr;
-			// 	NodePtr x = this->root;
-	
-			// 	while (x != TNULL)
-			// 	{
-			// 		y = x;
-			// 		if (node->data < x->data)
-			// 			x = x->left;
-			// 		else
-			// 			x = x->right;
-			// 	}
-	
-			// 	node->parent = y;
-			// 	if (y == my_nullptr)
-			// 		root = node;
-			// 	else if (node->data < y->data)
-			// 		y->left = node;
-			// 	else
-			// 		y->right = node;
-	
-			// 	if (node->parent == my_nullptr)
-			// 	{
-			// 		node->color = BLACK;
-			// 		return ;
-			// 	}
-	
-			// 	if (node->parent->parent == my_nullptr)
-			// 		return ;
-	
-			// 	insertFix(node);
-			// }
 	
 			NodePtr getRoot()
 			{
